b5166989ba17e37f456f0e4d75643f545134dac7
Felipe Flor <f.flor@vehikl.com>
2019-03-17 23:56:33 -0400

    Initial Commit

---
+++ resources/assets/ts/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Vue Typescript is Working!</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from 'vue-property-decorator'
+
+    @Component
+    export default class ExampleComponent extends Vue {
+        mounted(): void {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Music Game</title>
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+</body>
+</html>
